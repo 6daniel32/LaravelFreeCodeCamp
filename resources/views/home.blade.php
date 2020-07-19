9b703e7443fb08e5e0dde66611a56a8b85ae8d4f
Daniel <6daniel32@gmail.com>
2020-07-19 19:36:28 +0000

    auth, database migrations and FrontEnd changes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img src="https://instagram.flpa1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.flpa1-1.fna.fbcdn.net&_nc_ohc=R4RWsNsSv3wAX-JG8Nq&oh=e97400aada9d5723aaf5cb702db04db2&oe=5F3EE567">
+        </div>
+        <div class="col-9"></div>
+    </div>
+</div>
+@endsection
